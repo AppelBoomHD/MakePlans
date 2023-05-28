164e2fb12c5299d8d17d76a114017fe0399e35a4
AppelBoomHD <julianriemersma@gmail.com>
2023-05-29 00:51:16 +0200

    fix(groups): add close button to group info view

--- MakePlans/Views/GroupInfoView.swift
+++ MakePlans/Views/GroupInfoView.swift
@@ -11,49 +11,57 @@ import SwiftUI
 struct GroupInfoView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @ObservedObject var groupVM: GroupViewModel
-    
-    @State var isShowingInviteView = false
-    
-    var body: some View {
-        VStack {
-            Text("Group:")
-                .font(.headline)
-                .bold()
-                .padding(.top, 40)
-            Text(groupVM.group.groupName)
-                .font(.headline)
-            Spacer()
-                .frame(height: 40)
-            Text("Owner:")
-                .font(.headline)
-            Text(groupVM.group.owner ?? "Unknown")
-            Spacer()
-                .frame(height: 40)
-            NavigationLink(destination: Text("Second View"), isActive: $isShowingInviteView) { EmptyView() }
-            Button("Invite a friend") {
-                self.isShowingInviteView = true
-            }
-            .buttonStyle(LightButtonStyle(paddingSize: 20, shape: RoundedRectangle(cornerRadius: 25)))
-            Spacer()
-            Button(action: {
-                self.groupVM.deleteGroup()
-                self.presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("Delete group")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 220, height: 60)
-                    .background(Color.red)
-                    .cornerRadius(15.0)
 
+    @State var isShowingInviteView = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                    .frame(height: 40)
+                Text("Owner:")
+                    .font(.headline)
+                Text(groupVM.group.owner ?? "Unknown")
+                Spacer()
+                    .frame(height: 40)
+                NavigationLink(destination: Text("Second View"), isActive: $isShowingInviteView) { EmptyView() }
+                Button("Invite a friend") {
+                    self.isShowingInviteView = true
+                }
+                .buttonStyle(LightButtonStyle(paddingSize: 20, shape: RoundedRectangle(cornerRadius: 25)))
+                Spacer()
+                Button(action: {
+                    self.groupVM.deleteGroup()
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Delete group")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 220, height: 60)
+                        .background(Color.red)
+                        .cornerRadius(15.0)
+                }
             }
+            .navigationTitle(groupVM.group.groupName)
+            .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Image(systemName: "xmark.circle.fill")
+                                .resizable()
+                                .frame(width: 25, height: 25)
+                        }
+                        .foregroundColor(.black)
+                    }
+                }
         }
     }
 }
 
-//struct GroupInfoView_Previews: PreviewProvider {
+// struct GroupInfoView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        GroupInfoView()
 //    }
-//}
+// }
